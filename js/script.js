f1baa43612d38c47a1f6902e51ea35f9da0a3685
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-10-07 17:34:29 -0700

    code refactoring

--- js/script.js
+++ js/script.js
@@ -8,19 +8,7 @@
             goingUp = offset < lastOffset;
             lastOffset = offset;
 
-        if (newTop > -400)
-            $(".call-out").css("top", newTop)
-
-        if (goingUp)
-        {
-            $(".site-header").css("top", 0)
-        }
-        else
-        {
-            if (newTop <= -150)
-                $(".site-header").css("top", -150 )
-            else
-                $(".site-header").css("top", 0)
-        }
+        $(".call-out").css("top", newTop)
+        $(".site-header").css("top", (newTop <= -150 && !goingUp) ? -150 : 0 )
     })
 })()
